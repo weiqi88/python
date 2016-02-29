07d985c03798feba943e55e22fd15d8d2d8163fa
Your Name <120779446@qq.com>
2016-02-29 18:37:26 +0800

    editor character

--- liao/character.py
+++ liao/character.py
@@ -1,1 +1,34 @@
+#coding=utf-8
 print 'i\'m\ "ok\"'
+print'I\'m learning\nPython'
+print '\\\n\\'
+print '\\\t\\'
+print r'\\\t\\'
+print '''line1
+line2
+line3
+'''
+
+print 3>2
+print 3>5
+print 3>2 or 3>1
+print 3>2 or 3>4
+print 3>4 or 3>4
+print not 3>2 
+print not 3>2
+print not 3>4
+print '............'
+if False:
+	print '123'
+else:
+	print 'abc'
+	
+a='ABC'
+b=a
+a='XYZ'
+print 'a='+a,'b='+b
+
+print u'ABC'
+
+print u'ABC'.encode('utf-8')
+print u"中文".encode('gbk')
